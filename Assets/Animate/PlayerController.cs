72059093a62ffe5544ef0904100064d9efe49986
Parth Vaidya <zeusparth99@gmail.com>
2024-10-11 19:27:44 +0530

    Crouch

--- Assets/Animate/PlayerController.cs
+++ Assets/Animate/PlayerController.cs
@@ -5,27 +5,42 @@ using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator animator;
+    [SerializeField] private Animator playerAnimator;
+    [SerializeField] private BoxCollider2D boxCol;
 
-    private void Awake()
+    //Collider Variables
+    private Vector2 boxColInitSize;
+    private Vector2 boxColInitOffset;
+
+    private void Start()
     {
-        Debug.Log("Player is awake");
+        //Fetching initial collider properties
+        boxColInitSize = boxCol.size;
+        boxColInitOffset = boxCol.offset;
     }
-    private void Update()
+
+    public void Crouch(bool crouch)
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed" , Mathf.Abs(speed));
-        Vector3 scale = transform.localScale;
-        if (speed <0 )
+        if (crouch == true)
         {
-            
-            scale.x = -1f * Mathf.Abs(scale.x);
-        }
-        else if (speed > 0) 
-        {
-            scale.x = Mathf.Abs(scale.x);
+            float offX = -0.0978f;     //Offset X
+            float offY = 0.5947f;      //Offset Y
+
+            float sizeX = 0.6988f;     //Size X
+            float sizeY = 1.3398f;     //Size Y
+
+            boxCol.size = new Vector2(sizeX, sizeY);   //Setting the size of collider
+            boxCol.offset = new Vector2(offX, offY);   //Setting the offset of collider
         }
 
-        transform.localScale = scale;
+        else
+        {
+            //Reset collider to initial values
+            boxCol.size = boxColInitSize;
+            boxCol.offset = boxColInitOffset;
+        }
+
+        //Play Crouch animation
+        playerAnimator.SetBool("Crouch", crouch);
     }
 }
