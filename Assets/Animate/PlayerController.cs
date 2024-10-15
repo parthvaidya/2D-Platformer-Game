81109089dc65e5579ed4fc7f6b4f835e261bcc5a
Parth Vaidya <zeusparth99@gmail.com>
2024-10-15 14:01:05 +0530

    Vertical input & Jump added

--- Assets/Animate/PlayerController.cs
+++ Assets/Animate/PlayerController.cs
@@ -5,38 +5,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Animator playerAnimator;
-    public float speed;
-    public float jumpForce = 7f; // Adjust jump force as needed
-    public bool isGrounded;
-    //Take a reference from inspector.
-   
 
-   
+    public float speed;
+    public float jump;
+
+    private Rigidbody2D rb2d;
+
+    //Take a reference from inspector.
+
+    private void Awake()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
 
     public void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        MoveCharacter(horizontal);
-        PlayMovementAnimation(horizontal);
+        float vertical = Input.GetAxisRaw("Jump");
+        MoveCharacter(horizontal , vertical);
+        PlayMovementAnimation(horizontal , vertical);
 
-        // Jump logic
-        if (Input.GetKeyDown(KeyCode.Space) )
-        {
-            Jump();
-        }
+       
 
 
     }
 
-    private void MoveCharacter(float horizontal)
+    private void MoveCharacter(float horizontal , float vertical)
     {
         Vector3 position = transform.position;
         position.x = position.x + horizontal * speed * Time.deltaTime;
         transform.position = position;
 
+        if (vertical > 0)
+        {
+            rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+        }
+
     }
 
-    private void PlayMovementAnimation(float horizontal)
+    private void PlayMovementAnimation(float horizontal , float vertical)
     {
         playerAnimator.SetFloat("Speed", Mathf.Abs(horizontal));
 
@@ -54,23 +62,19 @@ public class PlayerController : MonoBehaviour
         }
         transform.localScale = scale;
 
-        Input.GetAxisRaw("Vertical");
-        Input.GetKeyDown(KeyCode.Space);
+        
+
+        if (vertical >0 )
+        {
+            playerAnimator.SetBool("Jump", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("Jump", false);
+        }
     }
 
-    private void Jump()
-    {
-        // Trigger the jump animation
-        playerAnimator.SetBool("Jump", true);
-
-       
-
-        
-        isGrounded = false;
-
-        
-    }
-
+   
     
 
 
