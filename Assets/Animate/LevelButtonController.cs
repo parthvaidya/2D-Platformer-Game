ce76b89e977453023923a62f965c0d765ade5597
Parth Vaidya <zeusparth99@gmail.com>
2024-10-19 19:45:30 +0530

    Added level locking unlocking

--- Assets/Animate/LevelButtonController.cs
+++ Assets/Animate/LevelButtonController.cs
@@ -2,9 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelButtonController : MonoBehaviour
 {
+
+    public Button[] button;
+
+    private void Awake()
+    {
+        // Ensure you are using the same key name
+        int levelAt = PlayerPrefs.GetInt("levelAt", 1);
+
+        for (int i = 0; i < button.Length; i++)
+        {
+            button[i].interactable = false;
+
+        }
+
+        for (int i = 0; i < levelAt; i++)
+        {
+            button[i].interactable = true;
+
+        }
+
+
+    }
     // Method for loading Level 1 (build index 1)
     public void LoadLevel1()
     {
--- Assets/Animate/LevelController.cs
+++ Assets/Animate/LevelController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour
 {
@@ -8,7 +9,28 @@ public class LevelController : MonoBehaviour
     {
         if (collision.gameObject.GetComponent<PlayerController>() != null) {
             Debug.Log("Level finished");
+            UnlockNewLevel();
             SceneController.instance.nextLevel();
         }
     }
+
+
+    void UnlockNewLevel()
+    {
+        // Get the current level index and the levelAt value
+        int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
+        int levelAt = PlayerPrefs.GetInt("levelAt", 1); 
+
+        // Check if the current level index is the last unlocked level
+        if (currentLevelIndex == levelAt)
+        {
+            PlayerPrefs.SetInt("levelAt", levelAt + 1); // Increment levelAt to unlock the next level
+            PlayerPrefs.Save(); // Save the changes
+            Debug.Log("Unlocked level: " + (levelAt + 1)); // Log the new unlocked level
+        }
+    }
+
 }
+
+
+
